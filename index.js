7b6df88ba815184f1d028fe9319f1fbac4bff509
Kirill <kirillsavorin@gmail.com>
2025-01-31 23:01:10 +0100

    put, delete

--- index.js
+++ index.js
@@ -28,6 +28,46 @@ app.get('/api/users', async (req, res) => {
     }
 });
 
+app.get('/api/user/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.put('/api/user/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const updatedUser = await User.findByIdAndUpdate(id, req.body);
+
+        if(!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser); 
+    } catch(err) { 
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.delete('/api/user/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        if(!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(`User ${deletedUser.name} has been deleted`); 
+    } catch(err) { 
+        res.status(500).json({ message: err.message });
+    }
+});
+
 mongoose.connect('mongodb+srv://kino:MHExXeNvqWBEb3PF@kinobackend.hhhzy.mongodb.net/Node-API?retryWrites=true&w=majority&appName=KinoBackend')
 .then(() => { console.log('Connected to MongoDB') })
 .catch((err) => { console.log('Failed to connect to MongoDB', err) });
