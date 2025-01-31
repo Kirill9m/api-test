37b901d795dfd15565a23d1cf7a80eb804b53076
Kirill <kirillsavorin@gmail.com>
2025-01-31 22:36:15 +0100

    models, mongooDB and api testing

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import User from './models/user.model.js';
 
 const app = express();
 
@@ -8,15 +10,28 @@ app.get('/', (req, res) => {
     res.send('Hello!');
 });
 
-app.post('/api/login', (req, res) => {
-    if(!req.body.name || !req.body.password) {
-        res.status(400).send('Name and password are required');
-    } else{
-    const name = req.body.name;
-    console.log("New user is logging in: " + name);
-    res.send('You are logged in');
+app.post('/api/register', async (req, res) => {
+    try{
+        const newUser = await User.create(req.body);
+        res.status(200).json(newUser);
+    } catch(err) {
+        res.status(500).json({ message: err.message });
     }
 });
+
+app.get('/api/users', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch(err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+mongoose.connect('mongodb+srv://kino:MHExXeNvqWBEb3PF@kinobackend.hhhzy.mongodb.net/Node-API?retryWrites=true&w=majority&appName=KinoBackend')
+.then(() => { console.log('Connected to MongoDB') })
+.catch((err) => { console.log('Failed to connect to MongoDB', err) });
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
